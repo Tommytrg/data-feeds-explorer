69073fbc74054fc8332e8a4db22a681cf01a95aa
Tomás Ruiz <tomastrg22@gmail.com>
2022-03-07 17:34:48 +0100

    wip

--- packages/api/src/resolvers.ts
+++ packages/api/src/resolvers.ts
@@ -1,66 +1,102 @@
 import { Context, Network } from './types'
+
+async function logTimeNeeded(log, fn) {
+  const before = Date.now()
+  const result = await fn()
+  const after= Date.now()
+
+  console.log(`${log} ${after - before}ms`)
+
+  return result
+
+}
+
 const resolvers = {
   Query: {
     feeds: async (_parent, args, { feedRepository }: Context) => {
-      return await feedRepository.getPaginatedFeeds(args.network)
+      return logTimeNeeded('Query.feeds', async () => {
+        return await feedRepository.getPaginatedFeeds(args.network)
+      })
     },
 
     networks: async (_parent, _args) => {
-      return Object.keys(Network).map(key => ({ label: Network[key] }))
+      return logTimeNeeded('Query.networks', async () => {
+        return Object.keys(Network).map(key => ({ label: Network[key] }))
+      })
     },
 
     requests: async (_parent, args, { resultRequestRepository }: Context) => {
-      return await resultRequestRepository.getFeedRequestsPage(
-        args.feedFullName,
-        args.page,
-        args.size
-      )
+      return logTimeNeeded('Query.requests', async () => {
+        return await resultRequestRepository.getFeedRequestsPage(
+          args.feedFullName,
+          args.page,
+          args.size
+        )
+      })
     },
 
     feed: async (_parent, args, { feedRepository }: Context) => {
-      return await feedRepository.get(args.feedFullName)
+      return logTimeNeeded('Query.feed', async () => {
+        return await feedRepository.get(args.feedFullName)
+      })
     }
   },
   Feed: {
     requests: async (parent, args, { resultRequestRepository }: Context) => {
-      return await resultRequestRepository.getFeedRequests(
-        parent.feedFullName,
-        args.timestamp
-      )
+      return logTimeNeeded('Feed,requests', async () => {
+        return await resultRequestRepository.getFeedRequests(
+          parent.feedFullName,
+          args.timestamp
+        )
+      })
     },
     lastResult: async (parent, _args, { resultRequestRepository }: Context) => {
-      // FIXME: add dataloader library to avoid overfetching
-      return (await resultRequestRepository.getLastResult(parent.feedFullName))
-        ?.result
+      return logTimeNeeded('Feed.lastResult', async () => {
+        // FIXME: add dataloader library to avoid overfetching
+        return (await resultRequestRepository.getLastResult(parent.feedFullName))?.result
+      })
     },
     lastResultTimestamp: async (
       parent,
       _args,
       { resultRequestRepository }: Context
     ) => {
-      // FIXME: add dataloader library to avoid overfetching
-      return (await resultRequestRepository.getLastResult(parent.feedFullName))
-        ?.timestamp
+      return logTimeNeeded('Feed.lastResultTimestamp', async () => {
+        // FIXME: add dataloader library to avoid overfetching
+        return (await resultRequestRepository.getLastResult(parent.feedFullName))?.timestamp
+      })
     },
     color: async (parent, _args, { config }: Context) => {
-      return config[parent.feedFullName]?.color || ''
+      return logTimeNeeded('Feed.color', async () => {
+        return config[parent.feedFullName]?.color || ''
+      })
     },
     blockExplorer: async (parent, _args, { config }: Context) => {
-      return config[parent.feedFullName]?.blockExplorer || ''
+      return logTimeNeeded('Feed.blockExplorer', async () => {
+        return config[parent.feedFullName]?.blockExplorer || ''
+      })
     },
     proxyAddress: async (parent, _args, { config }: Context) => {
-      return config[parent.feedFullName]?.routerAddress || ''
+      return logTimeNeeded('Feed.proxyAddress', async () => {
+        return config[parent.feedFullName]?.routerAddress || ''
+      })
     },
     deviation: async (parent, _args, { config }: Context) => {
-      return config[parent.feedFullName]?.deviation || ''
+      return logTimeNeeded('Feed.deviation', async () => {
+        return config[parent.feedFullName]?.deviation || ''
+      })
     },
     heartbeat: async (parent, _args, { config }: Context) => {
-      // Heartbeat plus aproximate time in milliseconds that takes to resolve the witnet dr
-      return config[parent.feedFullName]?.heartbeat || ''
+      return logTimeNeeded('Feed.heartbeat', async () => {
+        // Heartbeat plus aproximate time in milliseconds that takes to resolve the witnet dr
+        return config[parent.feedFullName]?.heartbeat || ''
+      })
     },
     finality: async (parent, _args, { config }: Context) => {
-      // Heartbeat plus aproximate time in milliseconds that takes to resolve the witnet dr
-      return config[parent.feedFullName]?.finality || ''
+      return logTimeNeeded('Feed.finality', async () => {
+        // Heartbeat plus aproximate time in milliseconds that takes to resolve the witnet dr
+        return config[parent.feedFullName]?.finality || ''
+      })
     }
   }
 }
